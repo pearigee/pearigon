8850f9c1f5543f50b456a5d9fe09962166df6baa
pearigee <pearigee@hey.com>
2022-02-03 23:00:42 -0800

    Build the linear algebra functions required for point transformations. #9

--- src/frontend/math.cljs
+++ src/frontend/math.cljs
@@ -1,16 +1,30 @@
 (ns frontend.math)
 
+(def pi js/Math.PI)
+
+(defn sin [x]
+  (js/Math.sin x))
+
+(defn cos [x]
+  (js/Math.cos x))
+
 (defn v+
-  [a b]
-  (mapv + a b))
+  ([a b]
+   (mapv + a b))
+  ([a b & more]
+   (reduce v+ (v+ a b) more)))
 
 (defn v-
-  [a b]
-  (mapv - a b))
+  ([a b]
+   (mapv - a b))
+  ([a b & more]
+   (reduce v- (v- a b) more)))
 
 (defn v*
-  [a b]
-  (mapv * a b))
+  ([a b]
+   (mapv * a b))
+  ([a b & more]
+   (reduce v* (v* a b) more)))
 
 (defn sum
   [vecs]
@@ -30,12 +44,48 @@
   [a b]
   (norm (v- a b)))
 
-(comment
-  (v+ [2 2] [1 1])
-  (v- [2 2] [1 1])
-  (v* [2 2] [2 2])
-  (norm [1 1]) ; -> 1.41...
-  (sum [[1 1] [2 2] [3 3] [4 4] [5 5]])
-  (avg [[1 1] [2 2] [3 3] [4 4] [5 5]])
-  (dist [1 1] [2 2]) ; -> 1.41
-  )
+(defn dot [a b]
+  (apply + (v* a b)))
+
+(defn mv* [m v]
+  ;; For our usecase, the last row of the 3x3 matrix is constant: [0 0 1].
+  ;; The same is true for the for the 3D vector, the final entry is 1.
+  ;; As a result, we can ignore the final row of the matrix.
+  (let [[[m1 m2 m3]
+         [m4 m5 m6]] m
+        [x y] v]
+    [(+ (* m1 x) (* m2 y) (* m3 1))
+     (+ (* m4 x) (* m5 y) (* m6 1))]))
+
+(defn mm*
+  ([a b]
+   ;; The final row is constant for our use case: [0 0 1]. As a result,
+   ;; we can ignore that row in the function below.
+   (let [[ar1 ar2] a
+         [[b1 b2 b3]
+          [b4 b5 b6]] b]
+     [[(dot ar1 [b1 b4 0])
+       (dot ar1 [b2 b5 0])
+       (dot ar1 [b3 b6 1])]
+      [(dot ar2 [b1 b4 0])
+       (dot ar2 [b2 b5 0])
+       (dot ar2 [b3 b6 1])]]))
+  ([a b & more]
+   (reduce mm* (mm* a b) more)))
+
+(defn transform
+  "Generates a transformation matrix from the input (apply left to right)."
+  [& txs]
+  (apply mm* (reverse txs)))
+
+(defn translate [x y]
+  [[1 0 x]
+   [0 1 y]])
+
+(defn scale [x y]
+  [[x 0 0]
+   [0 y 0]])
+
+(defn rotate [rad]
+  [[(cos rad) (sin rad) 0]
+   [(- (sin rad)) (cos rad) 0]])
