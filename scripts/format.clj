743eed13c745a91dc6a350c181fe5986c23e68de
pearigee <pearigee@hey.com>
2022-02-27 15:17:17 -0800

    Improved format script (bump LSP pod version).

--- scripts/format.clj
+++ scripts/format.clj
@@ -2,8 +2,12 @@
   (:require
    [babashka.pods :as pods]))
 
-(pods/load-pod 'com.github.clojure-lsp/clojure-lsp "2022.01.03-19.46.10")
+(pods/load-pod 'com.github.clojure-lsp/clojure-lsp "2022.02.01-20.02.32")
 
 (require '[clojure-lsp.api :as lsp])
 
+(lsp/clean-ns! {})
 (lsp/format! {})
+
+;; Return done to prevent printing the giant result object from format.
+"Done."
