b0fa698f2fe933a5217ae2ffb2e9ea6d080e6949
pearigee <pearigee@hey.com>
2022-02-27 11:11:33 -0800

    Sidebar cleanup. Remove dep on @tabler/icons.

--- src/frontend/view/sidebar.cljs
+++ src/frontend/view/sidebar.cljs
@@ -1,18 +1,16 @@
 (ns frontend.view.sidebar
-  (:require
-   ["@tabler/icons" :rename {IconX icon-minimize}]
-   [frontend.state.viewport :as viewport]
-   [frontend.view.panels.styles-panel :refer [styles-panel]]))
+  (:require [frontend.state.viewport :as viewport]
+            [frontend.view.panels.styles-panel :refer [styles-panel]]))
 
 (defn sidebar-panel
   [title content]
-  [:div.sidebar.notification
+  [:div.sidebar
    [:div.sidebar-header
     [:strong title]
     [:button.button.is-small
      {:on-click #(viewport/panel! nil)}
-     [:span.icon.is-small [:> icon-minimize]]]]
-   content])
+     "Close"]]
+   [:div.sidebar-content content]])
 
 (defn sidebar []
   (let [panel (viewport/panel)]
