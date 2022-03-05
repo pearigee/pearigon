740d206b7fda9b5c8002127ea87c1b899d85b0fc
pearigee <pearigee@hey.com>
2022-03-04 19:57:51 -0800

    Reuse utils.edn in tools.open.

--- src/frontend/tools/open.cljs
+++ src/frontend/tools/open.cljs
@@ -1,15 +1,11 @@
 (ns frontend.tools.open
   (:require
-   [clojure.edn :as edn]
-   [frontend.shapes.path.path :refer [map->Path]]
-   [frontend.shapes.path.point :refer [map->Point]]
+   [frontend.utils.edn :as edn]
    [frontend.state.core :as state]
    [frontend.utils.file-system :as fs]))
 
 (defn open []
   (fs/open-project
    (fn [file]
-     (let [readers {'frontend.shapes.path.path.Path map->Path
-                    'frontend.shapes.path.point.Point map->Point}
-           result (edn/read-string {:readers readers} file)]
+     (let [result (edn/read-string file)]
        (state/apply-save-state! result)))))
